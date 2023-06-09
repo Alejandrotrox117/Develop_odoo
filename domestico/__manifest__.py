f261758dd99c6c7bf651560e78f9285cee6d35c1
Alejandrotrox117 <gilalejandro926@gmail.com>
2023-06-09 15:46:10 -0400

    guardo para cambiar de rama

--- domestico/__manifest__.py
+++ domestico/__manifest__.py
@@ -25,11 +25,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        #'views/views.xml',
         'views/res_partner_views.xml',
-        'views/templates.xml',
         'views/product_template_view.xml',
-    ],
+        ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
