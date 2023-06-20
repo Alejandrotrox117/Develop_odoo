f7d8091916bf92d6e786def1726acf1f66ef46ea
samueldavidra17 <samueldavidra@gmail.com>
2023-06-20 19:19:43 -0400

    [update] new model product assigment with pivot view

--- domestico/__manifest__.py
+++ domestico/__manifest__.py
@@ -4,10 +4,11 @@
     'author': "My Company",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['base','product', 'account', 'contacts', 'sale'],
+    'depends': ['base','product', 'stock', 'account', 'contacts', 'sale'],
     'data': [
         'security/domestico_groups.xml',
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'views/domestico_product_assignment_views.xml',
         'views/res_partner_views.xml',
         'views/product_template_view.xml',
         'views/domestico_menus.xml',
