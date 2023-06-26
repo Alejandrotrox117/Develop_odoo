d1106ae10ee89552219b64000bd883579ee39c17
samueldavidra17 <samueldavidra@gmail.com>
2023-06-25 22:56:34 -0400

    [update] use sale_order_line in product_assingment views

--- domestico/__manifest__.py
+++ domestico/__manifest__.py
@@ -4,14 +4,15 @@
     'author': "My Company",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['base','product', 'stock', 'account', 'contacts', 'sale'],
+    'depends': ['base', 'stock', 'account', 'contacts', 'sale'],
     'data': [
         'security/domestico_groups.xml',
         'security/ir.model.access.csv',
-        'views/domestico_product_assignment_views.xml',
+
         'views/res_partner_views.xml',
         'views/product_template_view.xml',
         'views/account_payment_view.xml',
+        'views/domestico_product_assignment_views.xml',
         'views/domestico_menus.xml',
 
         'wizard/account_payment_register_view.xml',
