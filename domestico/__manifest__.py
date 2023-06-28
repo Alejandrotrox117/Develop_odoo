e7c9734679e539bf32c363cc4858bf5a3418bf76
samueldavidra17 <samueldavidra@gmail.com>
2023-06-28 10:34:29 -0400

    [new] Domestico theme

--- domestico/__manifest__.py
+++ domestico/__manifest__.py
@@ -4,7 +4,7 @@
     'author': "My Company",
     'category': 'Uncategorized',
     'version': '0.1',
-    'depends': ['base', 'stock', 'account', 'contacts', 'sale'],
+    'depends': ['base', 'stock', 'account', 'contacts', 'sale', 'hide_inbox_chatter', 'web_no_auto_save'],
     'data': [
         'security/domestico_groups.xml',
         'security/ir.model.access.csv',
@@ -17,8 +17,7 @@
         'views/product_pricelist_views.xml',
         'views/domestico_menus.xml',
         'views/views_menus_items.xml',
-        'views/res_partner_bank_views.xml',
-        
+        'views/res_partner_bank_views.xml',        
 
         'wizard/account_payment_register_view.xml',
     ],
@@ -26,5 +25,11 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'assets': {
+        'web._assets_primary_variables': [
+            #Load theme before default primary variables 
+            ('before', 'web/static/src/scss/primary_variables.scss', 'domestico/static/src/scss/domestico_theme.scss'),
+        ]
+    },
     'post_init_hook': 'create_update_bcv_job'
 }
