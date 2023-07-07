ece5254cb78293a08c0f5b7a163205c246005b1b
Alejandrotrox117 <gilalejandro926@gmail.com>
2023-07-06 22:38:30 -0400

    Termino reportes, agrego cubicaje a variantes de productos

--- domestico_stock/models/product_template.py
+++ domestico_stock/models/product_template.py
@@ -7,4 +7,8 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     taxes_id = fields.Many2many(default=[])
-    cubicaje=fields.Float(string="Cubicaje")
+    cubicaje=fields.Float(string="Cubicaje (mts)")
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    cubicaje=fields.Float(string="Cubicaje (mts)")
