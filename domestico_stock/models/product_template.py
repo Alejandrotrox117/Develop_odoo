199605d44501c362f969c74335e2aa94bd7e8c4a
Alejandrotrox117 <gilalejandro926@gmail.com>
2023-07-09 21:38:56 -0400

    Se creo reporte de grafico para proveedores

--- domestico_stock/models/product_template.py
+++ domestico_stock/models/product_template.py
@@ -13,4 +13,4 @@ class ProductTemplate(models.Model):
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     cubicaje=fields.Float(string="Cubicaje (mts)")
-    customer_id = fields.Many2one('res.partner', string="Proveedor")
+    customer_id = fields.Many2one('res.partner',related="product_tmpl_id.customer_id" ,string="Proveedor")
