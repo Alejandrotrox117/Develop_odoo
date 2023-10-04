c2b9dda3b0ec26a1c0d68f1d71ee54ba87b3ff3e
samueldavidra17 <samueldavidra@gmail.com>
2023-10-04 14:21:04 -0400

    [update] stock count product calcule with order

--- domestico/models/sale_order_line.py
+++ domestico/models/sale_order_line.py
@@ -3,9 +3,10 @@ from odoo import _, api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    product_virtual_available = fields.Float(string='Disponible', compute='_compute_product_virtual_available')
+    product_stock = fields.Float(string='Cantidad Restante', compute='_compute_product_stock')
 
     @api.depends('product_id')
-    def _compute_product_virtual_available(self):
+    def _compute_product_stock(self):
         for record in self:
-            record.product_virtual_available = record.product_id.virtual_available
+            order_product_count = sum(self.search([['product_id', '=', record.product_id.id]]).mapped('product_uom_qty'))
+            record.product_stock = record.product_id.free_qty - order_product_count
